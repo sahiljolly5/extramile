b275e721d7dfa8a9b178da26200997fd87e7ff64
sahil jolly <sahiljolly5@gmail.com>
2021-09-03 12:28:15 +0530

    save

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("EXTRAMILE");
+const express = require('express')
+const app = express()
+
+const routes = require('./routes/route')
+
+
+app.use('/public',express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+app.set('views','views')
+app.set('view engine','ejs')
+
+app.use('/',routes)
+
+app.listen(8080,(req,res) => {
+    console.log("Server Running");
+})
