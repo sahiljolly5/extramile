347eba7f691f0e1b798895d01f146d318fbe3e4b
sahil jolly <sahiljolly5@gmail.com>
2021-09-10 12:42:43 +0530

    today

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,11 @@ const router = require('express').Router()
 const User = require('../models/user')
 
 router.get('/',(req,res) => {
-    res.render('signin')
+    res.render('signin',{err:''})
+})
+
+router.get('/home',(req,res) => {
+    res.render('home')
 })
 
 router.get('/signup',(req,res) => {
@@ -14,7 +18,14 @@ router.post('/postSignin',async (req,res) => {
     var email = req.body.username;
     var password = req.body.password
     const result = await User.find({email:email,password:password})
-    console.log(result);
+    if(result.length > 0 ) {
+        res.redirect('home')
+    }
+    else {
+        res.render('signin',{err:'Wrong Email or password'})
+        // console.log("Wrong email or password");
+    }
+    // console.log(result);
 
     // res.render('signup')
 })
@@ -30,6 +41,10 @@ router.post('/postSignup',async (req,res) => {
     const result = await new User(data).save()
     // console.log(result);
     // res.render('signup')
+})
+
+router.post('/postDataSave',async (req,res) => {
+    console.log(req.body);
 })
 
 
